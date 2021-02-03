a9be46b8fe2ceca0f1d56e290b3d9db6650eceb8
rebeccamuller <rebeccamuller@rebeccas-air>
2021-02-03 18:23:14 +0100

    Erste lauffähige Version

---
+++ src/main/java/com/amazon/customskill/AlexaSkillSpeechlet.java
@@ -0,0 +1,356 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+package com.amazon.customskill;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.speechlet.SpeechletV2;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SsmlOutputSpeech;
+
+/*
+ * This class is the actual skill. Here you receive the input and have to produce the speech output. 
+ */
+public class AlexaSkillSpeechlet implements SpeechletV2 {
+	// Initialisiert den Logger. Am besten möglichst of Logmeldungen erstellen,
+	// hilft hinterher bei der Fehlersuche!
+	static Logger logger = LoggerFactory.getLogger(AlexaSkillSpeechlet.class);
+
+	// Variablen, die wir auch schon in DialogOS hatten
+	static int sum;
+	static String antwort1 = "";
+	static String antwort2 = "";
+	static String antwort3 = "";
+	static String antwort4 = "";
+	static String frage = "";
+	static UserIntent correctAnswer = null;
+
+	// Was der User gesagt hat
+	public static String userRequest;
+
+	// In welchem Spracherkennerknoten sind wir?
+	static enum RecognitionState {
+		Anfangsfrage, Quizfrage
+	};
+
+	RecognitionState recState;
+
+	static String userResponse = "";
+
+	// Was hat der User grade gesagt. (Die "Semantic Tags"aus DialogOS)
+	static enum UserIntent {
+		A, B, C, D, Error, Vokabeln, Saetze, Abbrechen
+	};
+
+	static enum VokabelnOderSaetze {
+		Vokabeln, Saetze
+	};
+
+	VokabelnOderSaetze status;
+
+	UserIntent ourUserIntent;
+
+	// Was das System sagen kann
+	Map<String, String> utterances;
+
+	// Baut die Systemäußerung zusammen
+	String buildString(String msg, String replacement1, String replacement2) {
+		return msg.replace("{replacement}", replacement1).replace("{replacement2}", replacement2);
+	}
+
+	// Liest am Anfang alle Systemäußerungen aus Datei ein
+	Map<String, String> readSystemUtterances() {
+		Map<String, String> utterances = new HashMap<String, String>();
+		try {
+			for (String line : IOUtils
+					.readLines(this.getClass().getClassLoader().getResourceAsStream("utterances.txt"))) {
+				if (line.startsWith("#")) {
+					continue;
+				}
+				String[] parts = line.split("=");
+				String key = parts[0].trim();
+				String utterance = parts[1].trim();
+				utterances.put(key, utterance);
+			}
+			logger.info("Read " + utterances.keySet().size() + "utterances");
+		} catch (IOException e) {
+			logger.info("Could not read utterances: " + e.getMessage());
+			System.err.println("Could not read utterances: " + e.getMessage());
+		}
+		return utterances;
+	}
+
+	// Datenbank für Quizfragen
+	private static Connection connection = null;
+
+	// Vorgegebene Methode wird am Anfang einmal ausgeführt, wenn ein neuer Dialog
+	// startet:
+	// * lies Nutzeräußerungen ein
+	// * Initialisiere Variablen
+	@Override
+	public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> requestEnvelope) {
+		logger.info("Alexa session begins");
+		utterances = readSystemUtterances();
+	}
+
+	// Wir starten den Dialog:
+	// * Hole die erste Frage aus der Datenbank
+	// * Lies die Welcome-Message vor, dann die Frage
+	// * Dann wollen wir eine Antwort erkennen
+	@Override
+	public SpeechletResponse onLaunch(SpeechletRequestEnvelope<LaunchRequest> requestEnvelope) {
+		logger.info("onLaunch");
+//		selectQuestion();
+		recState = RecognitionState.Anfangsfrage;
+		return askUserResponse(
+				"Schön, dass du da bist. Ich bin dein persönlicher Vokabeltrainer. Du kannst zu Beginn zwischen einzelnen Vokabeln und ganzen Sätzen wählen, die ich dich dann abfrage. Zu jeder Frage erhälst du vier Antwortmöglichkeiten. Du kannst mit den Buchstaben A, B,C, D antworten. Wenn du etwas nicht verstanden hast, frage einfach nach ob ich es wiederholen kann. Möchtest du Vokabeln oder Sätze lernen?");
+	}
+
+	// Hier gehen wir rein, wenn der User etwas gesagt hat
+	// Wir speichern den String in userRequest, je nach recognition State reagiert
+	// das System unterschiedlich
+	@Override
+	public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
+		IntentRequest request = requestEnvelope.getRequest();
+		Intent intent = request.getIntent();
+		userRequest = intent.getSlot("anything").getValue();
+		logger.info("Received following text: [" + userRequest + "]");
+		logger.info("recState is [" + recState + "]");
+
+		recognizeUserIntent(userRequest);
+		if (ourUserIntent == UserIntent.Abbrechen) {
+			return tellUserAndFinish("Bis zum nächsten Mal.");
+		}
+		if (ourUserIntent == UserIntent.Error) {
+			return tellUserAndFinish(
+					"Ich habe deine Eingabe nicht verstanden. Du hast folgendes gesagt " + userResponse);
+		}
+		switch (recState) {
+		case Anfangsfrage:
+			return beantworteAnfangsfrage(userRequest);
+
+		case Quizfrage:
+			return beantworteQuizfrage(userRequest);
+
+		}
+		return tellUserAndFinish("On Intent ");
+	}
+
+	private SpeechletResponse beantworteQuizfrage(String userRequest) {
+
+		if (ourUserIntent == correctAnswer) {
+
+			return stelleEineFrage("Super. Das war richtig. ");
+
+		} else {
+			return stelleEineFrage("Das war leider falsch. ");
+		}
+
+	}
+
+	private SpeechletResponse beantworteAnfangsfrage(String userRequest) {
+
+		if (ourUserIntent == UserIntent.Saetze) {
+			status = VokabelnOderSaetze.Saetze;
+			return stelleEineFrage("");
+		}
+		if (ourUserIntent == UserIntent.Vokabeln) {
+			status = VokabelnOderSaetze.Vokabeln;
+			return stelleEineFrage("");
+		}
+		return tellUserAndFinish("Dein Userintent ist: " + ourUserIntent);
+	}
+
+	private SpeechletResponse stelleEineFrage(String prefix) {
+		recState = RecognitionState.Quizfrage;
+		String typ = "";
+		if (status == VokabelnOderSaetze.Saetze) {
+			typ = "Sätze";
+		}
+		if (status == VokabelnOderSaetze.Vokabeln) {
+			typ = "Vokabeln";
+		}
+
+		try {
+			connection = DBConnection.getConnection();
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from " + typ);
+
+			LinkedList<Frage> fragen = new LinkedList<Frage>();
+
+			while (rs.next()) {
+
+				Frage frageObject = new Frage();
+				frageObject.setFrage(rs.getString("Frage"));
+				frageObject.setRichtig(rs.getString("Richtig"));
+				frageObject.setAntwort1(rs.getString("Antwort 1"));
+				frageObject.setAntwort2(rs.getString("Antwort 2"));
+				frageObject.setAntwort3(rs.getString("Antwort 3"));
+				frageObject.setAntwort4(rs.getString("Antwort 4"));
+				fragen.add(frageObject);
+			}
+
+			int randomNum = ThreadLocalRandom.current().nextInt(0, fragen.size() - 1);
+			Frage selectedQuestion = fragen.get(randomNum);
+			frage = selectedQuestion.getFrage();
+			switch (selectedQuestion.getRichtig()) {
+			case "A":
+				correctAnswer = UserIntent.A;
+				break;
+			case "B":
+				correctAnswer = UserIntent.B;
+				break;
+			case "C":
+				correctAnswer = UserIntent.C;
+				break;
+			case "D":
+				correctAnswer = UserIntent.D;
+				break;
+			}
+			antwort1 = selectedQuestion.getAntwort1();
+			antwort2 = selectedQuestion.getAntwort2();
+			antwort3 = selectedQuestion.getAntwort3();
+			antwort4 = selectedQuestion.getAntwort4();
+
+			return askUserResponse(prefix + "Was bedeutet " + frage + " auf Englisch? Antwort a: " + antwort1
+					+ ", Antwort b: " + antwort2 + ", Antwort c: " + antwort3 + ", Antwort d: " + antwort4);
+		} catch (Exception e) {
+			return tellUserAndFinish(e.getMessage());
+		}
+
+	}
+
+	// Achtung, Reihenfolge ist wichtig!
+	void recognizeUserIntent(String userRequest) {
+		userRequest = userRequest.toLowerCase();
+		userResponse = userRequest;
+		String pattern1 = "(ich nehme )?(antwort )?(\\b[a-d]\\b)( bitte)?";
+		String pattern2 = "(vokabeln){1}";
+		String pattern3 = "(sätze){1}";
+		String pattern4 = "(ich möchte )?(aufhören){1}";
+		String pattern5 = "(ich möchte )?(quizo)(beenden){1}";
+		String pattern6 = "(ich habe )? (keine lust mehr )(quizo zu spielen)?";
+		String pattern7 = "(ich möchte )?(vokabeln )(heute )?(lernen)?";
+		String pattern8 = "(ich möchte )?(heute )?(sätze )(lernen)?";
+		
+
+		Pattern p1 = Pattern.compile(pattern1);
+		Matcher m1 = p1.matcher(userRequest);
+		Pattern p2 = Pattern.compile(pattern2);
+		Matcher m2 = p2.matcher(userRequest);
+		Pattern p3 = Pattern.compile(pattern3);
+		Matcher m3 = p3.matcher(userRequest);
+		Pattern p4 = Pattern.compile(pattern4);
+		Matcher m4 = p4.matcher(userRequest);
+		Pattern p5 = Pattern.compile(pattern5);
+		Matcher m5 = p5.matcher(userRequest);
+		Pattern p6 = Pattern.compile(pattern6);
+		Matcher m6 = p6.matcher(userRequest);
+		Pattern p7 = Pattern.compile(pattern7);
+		Matcher m7 = p7.matcher(userRequest);
+		Pattern p8 = Pattern.compile(pattern8);
+		Matcher m8 = p8.matcher(userRequest);
+		
+
+		if (m1.find()) {
+			String answer = m1.group(3);
+			switch (answer) {
+			case "a":
+				ourUserIntent = UserIntent.A;
+				break;
+			case "b":
+				ourUserIntent = UserIntent.B;
+				break;
+			case "c":
+				ourUserIntent = UserIntent.C;
+				break;
+			case "d":
+				ourUserIntent = UserIntent.D;
+				break;
+			}
+		} else if (m2.find()) {
+			ourUserIntent = UserIntent.Vokabeln;
+		} else if (m7.find()) {
+			ourUserIntent = UserIntent.Vokabeln;
+		} else if (m3.find()) {
+			ourUserIntent = UserIntent.Saetze;
+		} else if (m8.find()) {
+			ourUserIntent = UserIntent.Saetze;
+		} else if (m4.find()) {
+			ourUserIntent = UserIntent.Abbrechen;
+		} else if (m5.find()) {
+			ourUserIntent = UserIntent.Abbrechen;
+		} else if (m6.find()) {
+			ourUserIntent = UserIntent.Abbrechen;
+		} else {
+			ourUserIntent = UserIntent.Error;
+		}
+		logger.info("set ourUserIntent to " + ourUserIntent);
+	}
+
+	@Override
+	public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> requestEnvelope) {
+		logger.info("Alexa session ends now");
+	}
+
+	/**
+	 * Tell the user something - the Alexa session ends after a 'tell'
+	 */
+	private SpeechletResponse tellUserAndFinish(String text) {
+		// Create the plain text output.
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText(text);
+
+		return SpeechletResponse.newTellResponse(speech);
+	}
+
+	/**
+	 * A response to the original input - the session stays alive after an ask
+	 * request was send. have a look on
+	 * https://developer.amazon.com/de/docs/custom-skills/speech-synthesis-markup-language-ssml-reference.html
+	 * 
+	 * @param text
+	 * @return
+	 */
+	private SpeechletResponse askUserResponse(String text) {
+		SsmlOutputSpeech speech = new SsmlOutputSpeech();
+		speech.setSsml("<speak>" + text + "</speak>");
+
+		// reprompt after 8 seconds
+		SsmlOutputSpeech repromptSpeech = new SsmlOutputSpeech();
+		repromptSpeech.setSsml("<speak><emphasis level=\"strong\">Hey!</emphasis> Bist du noch da?</speak>");
+
+		Reprompt rep = new Reprompt();
+		rep.setOutputSpeech(repromptSpeech);
+		return SpeechletResponse.newAskResponse(speech, rep);
+	}
+
+}
---
+++ src/main/java/com/amazon/customskill/Frage.java
@@ -0,0 +1,58 @@
+package com.amazon.customskill;
+
+public class Frage {
+	public String getFrage() {
+		return frage;
+	}
+
+	public void setFrage(String frage) {
+		this.frage = frage;
+	}
+
+	public String getAntwort1() {
+		return antwort1;
+	}
+
+	public void setAntwort1(String antwort1) {
+		this.antwort1 = antwort1;
+	}
+
+	public String getAntwort2() {
+		return antwort2;
+	}
+
+	public void setAntwort2(String antwort2) {
+		this.antwort2 = antwort2;
+	}
+
+	public String getAntwort3() {
+		return antwort3;
+	}
+
+	public void setAntwort3(String antwort3) {
+		this.antwort3 = antwort3;
+	}
+
+	public String getAntwort4() {
+		return antwort4;
+	}
+
+	public void setAntwort4(String antwort4) {
+		this.antwort4 = antwort4;
+	}
+
+	public String getRichtig() {
+		return richtig;
+	}
+
+	public void setRichtig(String richtig) {
+		this.richtig = richtig;
+	}
+
+	public String frage;
+	public String antwort1;
+	public String antwort2;
+	public String antwort3;
+	public String antwort4;
+	public String richtig;
+}
